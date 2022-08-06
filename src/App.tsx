a591dacf684db1ae8b61979fe3be7df49a7eb57f
Diego Dev <diegodev@Diegos-MacBook-Air.local>
2022-08-05 21:47:05 -0400

    adds routing
    e2e tests for redirect to login are currently failing; we've
    been in the midst of learning about react-router-dom and
    appropriate unit testing strategies.
    
    Signed-off-by: Diego Moura <diegoo.demoura@gmail.com>
    Co-authored-by: Jamie Klassen <jklassen@vmware.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  
+	return (
+		<main>
+			<section>
+				<p>please enter username and password</p>
+			</section>
+		</main>
+	);
 }
 
 export default App;
--- src/e2e/App.test.ts
+++ src/e2e/App.test.ts
@@ -1,22 +1,30 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer';
+import * as expectPage from 'expect-puppeteer';
 
 let browser: puppeteer.Browser | undefined;
 let page: puppeteer.Page | undefined;
 
 beforeAll(async () => {
-  browser = await puppeteer.launch({
-    headless: true
-  });
-  page = await browser.newPage();
-  await page.goto("http://localhost:3000");
+	browser = await puppeteer.launch({
+		headless: true,
+	});
+	page = await browser.newPage();
+	await page.goto('http://localhost:3000');
 });
 
-test("Page loads", async () => {
-  if (page === undefined) {
-    fail();
-  }
-  const root = await page.$("#root");
-  expect(root).not.toBeNull();
+test('Prompt to login visible', async () => {
+	if (page === undefined) {
+		fail();
+	}
+	await expectPage.default(page).toMatch('please enter username and password');
+});
+
+test('Redirects to logging page', async () => {
+	if (page === undefined) {
+		fail();
+	}
+	const url = page.url();
+	expect(url).toMatch('login');
 });
 
 afterAll(async () => await browser?.close?.());
